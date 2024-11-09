d38ac1b5101bcfb98ade33f5e6e383db66434782
Zhenghong Yu <zyu379@wisc.edu>
2024-11-09 16:23:58 -0600

    feat(Hypergraph.Basic): acylic test

--- Hypergraph/Basic.lean
+++ Hypergraph/Basic.lean
@@ -1,4 +1,14 @@
 import Mathlib.Data.Set.Basic
 import Mathlib.Data.Set.Pairwise.Basic
 
+structure Hypergraph (α : Type) where
+  nodes : Set α
+  hyperedges : Set (Set α)
+
+def setsIntersect {α : Type} (s1 s2 : Set α) : Prop :=
+  (s1 ∩ s2).Nonempty
+
+-- def isAcyclic {α : Type} [DecidableEq α] (hg : Hypergraph α) : Bool :=
+--   hg.hyperedges.Pairwise (λ e1 e2 => ¬ setsIntersect e1 e2)
+
 def hello := "world"
--- Main.lean
+++ Main.lean
@@ -1,4 +1,6 @@
 import Hypergraph
 
 def main : IO Unit :=
+  let hg := Hypergraph.mk {1, 2, 3, 4} {{1, 2}, {3, 4}, {2, 3}}
   IO.println s!"Hello, {hello}!"
+  -- IO.println s!"Hypergraph is acyclic: {isAcyclic hg}"
