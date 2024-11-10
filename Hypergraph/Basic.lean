48d8f23ed28f0ec2d33c95440495ee8d4073027f
Zhenghong Yu <zyu379@wisc.edu>
2024-11-09 20:09:46 -0600

    feat(Computable Hyptergraph): add some function

--- Hypergraph/Basic.lean
+++ Hypergraph/Basic.lean
@@ -3,6 +3,8 @@ import Mathlib.Data.Set.Pairwise.Basic
 import Mathlib.Data.Set.Card
 import Mathlib.Data.Finset.Basic
 
+open Finset
+
 structure ComputableHypergraph (α : Type) [DecidableEq α] where
   nodes : Finset α
   hyperedges : Finset (Finset α)
@@ -23,6 +25,30 @@ def createHyperedge {α : Type} [DecidableEq α] (nodes : List α) : Finset α :
 def addEdge [DecidableEq ℕ] (hg : ComputableHypergraph ℕ) (edge : Finset ℕ) : ComputableHypergraph ℕ :=
   { nodes := hg.nodes,
     hyperedges := insert edge hg.hyperedges }
+
+-- GYO algorithm --
+def findIsolatedVertices [DecidableEq α] (hg : ComputableHypergraph α) : Finset α :=
+  hg.nodes.filter (λ n => (hg.hyperedges.filter (λ e => n ∈ e)).card = 1)
+
+def removeIsolatedVertex [DecidableEq α] (hg : ComputableHypergraph α) (v : α) : ComputableHypergraph α :=
+  let updatedHyperedges : Finset (Finset α) := hg.hyperedges.image (λ e => e.erase v)
+  let nonEmptyHyperedges : Finset (Finset α) := updatedHyperedges.filter (λ e => e.Nonempty)
+  { nodes := hg.nodes.erase v,
+    hyperedges := nonEmptyHyperedges }
+
+-- def removeRedundantHyperedges [DecidableEq α] (hg : ComputableHypergraph α) : ComputableHypergraph α :=
+--   let redundantEdges := hg.hyperedges.filter (λ e => hg.hyperedges.any (λ f => e < f))
+--   { nodes := hg.nodes,
+--     hyperedges := hg.hyperedges.filter (λ e => ¬redundantEdges.contains e) }
+
+-- def gyoAlgorithm [DecidableEq α] (hg : ComputableHypergraph α) : Bool :=
+--   let rec loop (g : ComputableHypergraph α) : ComputableHypergraph α :=
+--     let isolated := findIsolatedVertices g
+--     let g := isolated.fold (λ acc v => removeIsolatedVertex acc v) g
+--     let gNew := removeRedundantHyperedges g
+--     if gNew = g then g else loop gNew
+--   let finalGraph := loop hg
+--   finalGraph.nodes.empty
 
 -- Below are test --
 
