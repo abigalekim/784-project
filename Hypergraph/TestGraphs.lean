99a75c15deb08fc3ab4f0c64e888112fdbabcce5
Zhenghong Yu <zyu379@wisc.edu>
2024-11-22 19:18:27 -0600

    test(paper test): alpha passed wow!!!!!!!!!!!!!!!!!!

--- Hypergraph/TestGraphs.lean
+++ Hypergraph/TestGraphs.lean
@@ -17,7 +17,7 @@ def braultBaronDHyperGraph : ComputableHypergraph ℕ :=
   { nodes := {0, 1, 2}, hyperedges := {{0, 1, 2}, {0, 1, 3}, {1, 2, 3}, {0, 2, 3}}}
 
 def braultBaronEHyperGraph : ComputableHypergraph ℕ :=
-  { nodes := {0, 1, 2}, hyperedges := {{0, 1}, {1, 2}, {2, 3}, {3, 0}}}
+  { nodes := {0, 1, 2, 3}, hyperedges := {{0, 1}, {1, 2}, {2, 3}, {3, 0}}}
 
 def braultBaronFHyperGraph : ComputableHypergraph ℕ :=
   { nodes := {0, 1, 2}, hyperedges := {{0, 1}, {0, 2}, {1, 2}}}
