9e4082f58e1591629d0bd1efe4156118108067e5
Skylar <wanzhu@Skylars-Air.attlocal.net>
2024-11-25 00:18:18 -0600

    Beta Acyclic

---
+++ Hypergraph/Beta.lean
@@ -0,0 +1,39 @@
+import Hypergraph.Basic
+import Hypergraph.TestGraphs
+open Finset
+
+def findVerticesNestEdge (α : Type) [DecidableEq α] (hg : ComputableHypergraph α) : Finset α :=
+  hg.nodes.filter (λ n =>
+    let edges := hg.hyperedges.filter (λ e => n ∈ e)
+    (edges.filter (λ e => ∃ other ∈ hg.hyperedges, (other ≠ e) ∧ (e ⊆ other))).card > 0
+    )
+
+def isBetaAcyclic (α : Type) [DecidableEq α] (hg : ComputableHypergraph α) : Bool :=
+  let rec loop (g : ComputableHypergraph α) (iter : Nat) : ComputableHypergraph α :=
+    match iter with
+    | 0 => g
+    | val + 1 =>
+      let old_g := g
+      -- remove vertices contained by nested edges
+      let nestedEdgeVertices := findVerticesNestEdge α g
+      let noNestedEdges := g.hyperedges.image (λ n => n \ nestedEdgeVertices)
+      let g : ComputableHypergraph α := { nodes := g.nodes \ nestedEdgeVertices, hyperedges := noNestedEdges }
+      -- remove empty hyperedges
+      let newHyperEdges := g.hyperedges.filter (λ e => e ≠ ∅)
+      let g := { nodes := g.nodes, hyperedges := newHyperEdges }
+
+      if old_g = g then g else loop g val
+
+  let maxIters : Nat := (hg.nodes.card + hg.hyperedges.card)
+  let finalGraph := loop hg maxIters
+  finalGraph.nodes = ∅
+
+--------------------
+-- Below are test --
+--------------------
+#eval isBetaAcyclic ℕ braultBaronAHyperGraph
+#eval isBetaAcyclic ℕ braultBaronBHyperGraph
+#eval isBetaAcyclic ℕ braultBaronCHyperGraph
+#eval isBetaAcyclic ℕ braultBaronDHyperGraph
+#eval isBetaAcyclic ℕ braultBaronEHyperGraph
+#eval isBetaAcyclic ℕ braultBaronFHyperGraph
