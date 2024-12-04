87b7252fbcbdfbe83d240213e15ff9618cf1f814
Abigale Kim <abigalekim0417@gmail.com>
2024-12-03 18:13:08 -0600

    stuff

--- Hypergraph/Gamma.lean
+++ Hypergraph/Gamma.lean
@@ -20,7 +20,7 @@ def isGammaAcyclic (α : Type) [DecidableEq α] (hg : ComputableHypergraph α) :
       let isolatedVertices := isolatedOneVertices ∪ isolatedZeroVertices
       let noIsolatedHyperEdges := g.hyperedges.image (λ n => n \ isolatedOneVertices)
       let g : ComputableHypergraph α := { nodes := g.nodes \ isolatedVertices, hyperedges := noIsolatedHyperEdges }
-      let newHyperEdges := g.hyperedges.filter (λ e => (g.hyperedges.filter (λ f =>  e ⊆ f ∨ e ∩ f = ∅)).card >= 1)
+      let newHyperEdges := g.hyperedges.filter (λ e => (g.hyperedges.filter (λ f => (e ⊆ f ∨ e ∩ f = ∅) ∧ e ≠ f)).card >= 1)
       let newHyperEdges := g.hyperedges \ newHyperEdges
       let g := { nodes := g.nodes, hyperedges := newHyperEdges }
 
@@ -33,7 +33,7 @@ def isGammaAcyclic (α : Type) [DecidableEq α] (hg : ComputableHypergraph α) :
 --------------------
 -- Below are test --
 --------------------
-#eval findVerticesNoHyperEdge ℕ braultBaronBHyperGraph
+#eval findVerticesNoHyperEdge ℕ braultBaronBHyperGraph ∪ findVerticesOneHyperEdge ℕ braultBaronBHyperGraph
 #eval isGammaAcyclic ℕ braultBaronAHyperGraph
 #eval isGammaAcyclic ℕ braultBaronBHyperGraph
 #eval isGammaAcyclic ℕ braultBaronCHyperGraph
