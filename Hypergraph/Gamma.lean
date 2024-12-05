bef17a318e3d6b250cffd216913b9c6c8e5a2740
Abigale Kim <abigalekim0417@gmail.com>
2024-12-05 03:17:29 -0600

    gamma cycle with prop instead of algo works

--- Hypergraph/Gamma.lean
+++ Hypergraph/Gamma.lean
@@ -1,8 +1,12 @@
 import Hypergraph.Basic
 import Hypergraph.TestGraphs
 import Mathlib.Tactic.Linarith
+import MathLib.Algebra.NeZero
 open Finset
 
+--set_option diagnostics true in
+-- is a type where a set of edges and vertices and an x that can satisfy all these conditions
+-- then there is an inhabitant of this type (there is a value of this type)
 structure GammaCycle (α : Type) [DecidableEq α] (G : ComputableHypergraph α) where
   n : Nat
   hn : n >= 3
@@ -10,10 +14,26 @@ structure GammaCycle (α : Type) [DecidableEq α] (G : ComputableHypergraph α) 
   E_distinct : ∀ i j : Fin n, E i ≠ E j
   x : Fin n -> α
   x_distinct : ∀ i j : Fin n, x i ≠ x j
-  cond_1 : ∀ i : Fin n,
-    let next := ⟨(i.val + 1) % n, Nat.mod_lt (i.val + 1) (by linarith [hn])⟩;
-    x i ∈ E i ∧ x next ∈ E next
-    -- for [1, n-1] we want to show that x i is not in any other E j other than the 2 in the cycle
+  --cond_1 : ∀ i : Fin n,
+  --  let next := ⟨(i.val + 1) % n, Nat.mod_lt (i.val + 1) (by linarith [hn])⟩;
+  --  x i ∈ E i ∧ x next ∈ E next
+  cond_1 : ∀ i : Nat, ∀ j : Nat, (i_lt : i < n - 1 ∧ j < n ∧ j ≠ i ∧ j ≠ i + 1) →
+    x ⟨i,by omega⟩ ∈ E ⟨i,by omega⟩ ∧
+    x ⟨i+1, by omega⟩ ∈ E ⟨i+1, by omega⟩ ∧
+    x ⟨i, by omega⟩ ∉ E ⟨j, by omega⟩
+  -- ∀ i ∈ {1, n-1}, ∀ j ∈ {1, n} where j ≠ i ∧ j ≠ i+1 -> x i ∉ E j
+  -- BETA: ∀ i ∈ {1, n}, ∀ j ∈ {1, n} where j ≠ i ∧ j ≠ i+1 -> x i ∉ E j
+  -- x (n-1) ∈ E (n-1) and x (n - 1) ∈ E 0
+  cond_2 : x ⟨n - 1, by omega⟩ ∈ E ⟨n - 1, by omega⟩ ∧ x ⟨n - 1, by omega⟩ ∈ E ⟨0, by omega⟩
+
+  --cond_2 : ∀ (i j : Fin n),
+  --  let next := ⟨(i.val + 1) % n, Nat.mod_lt (i.val + 1) (by linarith [hn])⟩;
+  --  x i ∉ E j
+
+  cond_4: ∀ i : Fin n, E i ∈ (G.hyperedges : Finset (Finset α))
+  cond_5: ∀ i : Fin n, x i ∈ (G.nodes : Finset α)
+
+def gamma_acyclic_v2 (α : Type) [DecidableEq α] (G : ComputableHypergraph α) := GammaCycle α G -> False
 
 def findVerticesNoHyperEdge (α : Type) [DecidableEq α] (hg : ComputableHypergraph α) :  Finset α :=
   let verticesInHyperedges : Finset α := hg.hyperedges.biUnion id
